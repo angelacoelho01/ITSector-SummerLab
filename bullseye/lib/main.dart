867b21ff10e683980133a47c89edc92bafaaea9f
Angela Coelho <angela.coelho.01@gmail.com>
2022-07-13 10:53:30 +0100

    Add slider

--- bullseye/lib/main.dart
+++ bullseye/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'prompt.dart';
+import 'control.dart';
+
 void main() {
   runApp(const BullsEyeApp());
 }
@@ -34,11 +37,8 @@ class _GamePageState extends State<GamePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Bullseye',
-              style:
-                  TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
-            ),
+            const Prompt(targetValue: 100),
+            const Control(),
             TextButton(
               onPressed: () {
                 _showAlert(context);
