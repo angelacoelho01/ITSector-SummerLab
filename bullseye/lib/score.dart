5b4b0b29ddc4e40c4d1549421c4999e39ad1a25d
Angela Coelho <angela.coelho.01@gmail.com>
2022-07-14 10:36:58 +0100

    Style text

--- bullseye/lib/score.dart
+++ bullseye/lib/score.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'text_styles.dart';
+
 class Score extends StatelessWidget {
   final int totalScore;
   final int round;
@@ -24,15 +26,33 @@ class Score extends StatelessWidget {
           child: const Text('Start Over'),
         ),
         Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: <Widget>[const Text('Score: '), Text('$totalScore')],
+          padding: const EdgeInsets.only(left: 32.0, right: 32.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Score: ',
+                style: LabelTextStyle.bodyText1(context),
+              ),
+              Text(
+                '$totalScore',
+                style: ScoreNumberTextStyle.headLine4(context),
+              )
+            ],
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: <Widget>[const Text('Round: '), Text('$round')],
+          padding: const EdgeInsets.only(left: 32.0, right: 32.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Round: ',
+                style: LabelTextStyle.bodyText1(context),
+              ),
+              Text(
+                '$round',
+                style: ScoreNumberTextStyle.headLine4(context),
+              )
+            ],
           ),
         ),
         TextButton(
